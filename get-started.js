94ec2f6351e0059818ebf07000f9a0b3d4fd674c
Nickolas Crim <email@nickolasc.com>
2024-02-22 10:05:30 -0500

    Create get-started.js

---
+++ get-started.js
@@ -0,0 +1,4 @@
+document.querySelector("#selector").addEventListener("change", function () {
+  var selector = document.querySelector(".form_input-select");
+  selector.style.color = "#fbf5ee";
+});
